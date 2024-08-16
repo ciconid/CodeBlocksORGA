fa7599a01d5771c27f46d175c62a72a4a9f7d8ef
ciconid <108319378+ciconid@users.noreply.github.com>
2024-08-16 15:58:58 -0300

    first commit

---
+++ elShamoun/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int p = 18;
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    return 0;
+}
