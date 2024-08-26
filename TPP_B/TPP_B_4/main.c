b95732d8611351e69fa1b9f04c39fabedb850e1e
ciconid <108319378+ciconid@users.noreply.github.com>
2024-08-26 16:56:51 -0300

    TPP_B_4 done

--- TPP_B/TPP_B_4/main.c
+++ TPP_B/TPP_B_4/main.c
@@ -15,12 +15,77 @@ a3 = *p;
 }
 
 
+int* trib(int x){
+int* res;
+*res = 3 * x;
+return res;
+}
+void b(){
+int a = 34;
+int a3;
+int* p;
+p = trib(a);
+a3 = *p;
+}
+
+
+int tric(int x){
+int res = 3 * x;
+return res;
+}
+void c(){
+int a = 34;
+int a3;
+a3 = tric(a);
+}
+
+
+void trid(int* x){
+int res;
+res = *x * 3;
+*x = res;
+}
+void d(){
+int a = 34;
+trid(&a);
+}
+
+
+void trie(int* x){
+int res;
+res = *x * 3;
+*x = res;
+}
+void e(){
+int* a;
+*a = 34;
+trie(a);
+}
+
+
+void trif(int* x){
+int res;
+res = *x * 3;
+*x = res;
+}
+void f(){
+int* a;
+int b;
+a = &b;
+*a = 34;
+trif(a);
+}
+
+
 
 int main()
 {
-    aaa();
-
-
+    //aaa();
+    //b();
+    //c();
+    //d();
+    //e();
+    f();
 
     return 0;
 }
