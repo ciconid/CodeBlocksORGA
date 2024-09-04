086805ea8d376e19315ea93de71132b89e0350b2
Cicolas Nid <ciconidips@gmail.com>
2024-09-04 18:45:51 -0300

    TPP_B_6 added

---
+++ TPP_B_6/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reemplazar(int arr[], int a, int n);
+void reemplazar(int* arr, int a, int n, int size);
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+void reemplazar(int arr[], int a, int n){
+
+}
+void reemplazar(int* arr, int a, int n, int size){
+
+}
---
+++ wall_flag_test/nombreNuevo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //int i;
+    printf("Hello wrlasdd!\n");
+    return 0;
+}
